8627b916278e3e94a81d61beeb4342b16f73c1b8
FranciscoVigo <79640958+FranciscoVigo@users.noreply.github.com>
2022-03-24 10:55:48 +0100

    Versión 3

--- src/PaqCommit/Main.java
+++ src/PaqCommit/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String[] args){
         A o1= new A(3, 7);
         System.out.println(o1.getA() + " "+ o1.getB());
+        A o2= new A (3,2);
+
     }
 }
