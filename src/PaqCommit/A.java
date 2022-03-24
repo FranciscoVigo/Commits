09e2e0798e632820032838c342c3236c6bc6eb0c
FranciscoVigo <79640958+FranciscoVigo@users.noreply.github.com>
2022-03-24 10:49:36 +0100

    Versión 2

--- src/PaqCommit/A.java
+++ src/PaqCommit/A.java
@@ -3,12 +3,22 @@ package PaqCommit;
 public class A {
 
     int a;
+    int b;
 
-    public A(int a){
+    public A(int a,int b){
         this.a=a;
+        this.b=b;
     }
 
     public int getA() {
         return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
     }
 }
--- src/PaqCommit/Main.java
+++ src/PaqCommit/Main.java
@@ -5,7 +5,7 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class Main {
 
     public static void main(String[] args){
-        A o1= new A(3);
-        System.out.println(o1.getA());
+        A o1= new A(3, 7);
+        System.out.println(o1.getA() + " "+ o1.getB());
     }
 }
